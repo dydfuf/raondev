4882d53d8cc87d40c3faa20b6d2f158b03062eb4
dydfuf <88dydfuf@naver.com>
2023-02-13 23:16:58 +0900

    tailwind css 적용

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div>
+          <p className="text-bold underline">Get started by editing</p>
+        </div>
+      </main>
+    </>
+  );
+}
