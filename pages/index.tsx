2e3feeac0f29d1a3e1940e34625933fd0e095386
dydfuf <88dydfuf@naver.com>
2023-02-16 23:27:53 +0900

    홈화면 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,37 @@
-export default function index() {
-  return <div>Home</div>;
+import fs from "fs";
+import { GetStaticProps } from "next";
+
+interface Props {
+  paths: { id: string }[];
 }
+
+export default function index({ paths }: Props) {
+  console.log({ paths });
+  return (
+    <div>
+      Home
+      <div>
+        {paths.map((path) => (
+          <li key={path.id}>{path.id}</li>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const posts = fs.readdirSync("posts").map((file) => file.split(".")[0]);
+
+    const paths = posts.map((post) => ({ id: post }));
+
+    return {
+      props: { paths },
+    };
+  } catch {
+    return {
+      props: {},
+      notFound: true,
+    };
+  }
+};
