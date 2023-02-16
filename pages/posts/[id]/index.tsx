221e2458f043fbb1240a990770fa55d029ea44ec
dydfuf <88dydfuf@naver.com>
2023-02-17 00:23:10 +0900

    fallback true

--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -8,16 +8,16 @@ export default function index(props: any) {
 }
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const posts = fs.readdirSync("./posts").map((file) => file.split(".")[0]);
+  const posts = fs.readdirSync("posts").map((file) => file.split(".")[0]);
 
   const paths = posts.filter((file) => file.match(/\.md$/)).map((post) => ({ params: { id: post } }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
   try {
-    const post = fs.readFileSync(`./posts/${context.params?.id}.md`).toString();
+    const post = fs.readFileSync(`posts/${context.params?.id}.md`).toString();
 
     return {
       props: { post },
