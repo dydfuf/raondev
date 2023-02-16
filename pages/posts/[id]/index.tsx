c8829d6944ccb0adddb3c93760d9870946253703
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2023-02-16 22:53:51 +0900

    마크다운 렌더러를 추가합니다. (#2)
    * tailwind css 적용
    
    * app 디렉토리 대신 pages사용하도록 수정
    
    * 마크다운 렌더러 추가

--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -1,19 +1,31 @@
+import fs from "fs";
+
+import Post from "@/components/Post";
 import { GetStaticPaths, GetStaticProps } from "next";
 
-export default function index() {
-  return <div>index</div>;
+export default function index(props: any) {
+  return <Post {...props} />;
 }
 
 export const getStaticPaths: GetStaticPaths = () => {
-  return {
-    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
-    fallback: false, // can also be true or 'blocking'
-  };
+  const posts = fs.readdirSync("posts").map((file) => file.split(".")[0]);
+
+  const paths = posts.filter((file) => file.match(/\.md$/)).map((post) => ({ params: { id: post } }));
+
+  return { paths, fallback: "blocking" };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  return {
-    // Passed to the page component as props
-    props: { post: {} },
-  };
+  try {
+    const post = fs.readFileSync(`posts/${context.params?.id}.md`).toString();
+
+    return {
+      props: { post },
+    };
+  } catch {
+    return {
+      props: {},
+      notFound: true,
+    };
+  }
 };
