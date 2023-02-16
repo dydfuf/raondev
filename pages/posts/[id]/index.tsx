13bb1f4d2a84502e0b87292657b70afc96502ec4
dydfuf <88dydfuf@naver.com>
2023-02-16 23:31:15 +0900

    fallback false로 변경

--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -12,7 +12,7 @@ export const getStaticPaths: GetStaticPaths = () => {
 
   const paths = posts.filter((file) => file.match(/\.md$/)).map((post) => ({ params: { id: post } }));
 
-  return { paths, fallback: "blocking" };
+  return { paths, fallback: false };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
