c3eaf48b1748002a9b29a2aa50fa0d492591fecd
dydfuf <88dydfuf@naver.com>
2023-02-23 23:00:30 +0900

    카드 디자인 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
+import ContentCard from "@/components/ContentCard";
 import { getPostList } from "@/utils/post";
-import fs from "fs";
 import { GetStaticProps } from "next";
 import Link from "next/link";
 
@@ -11,6 +11,58 @@ export default function index({ paths }: Props) {
   return (
     <div>
       <div>
+        <div className="w-full gap-20 grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 p-20">
+          <ContentCard
+            title="this is title"
+            timeToRead="10min"
+            description="this is description hello world!"
+            releaseDate="2023-02-21"
+            hashtags={["hash", "tag"]}
+          />
+          <ContentCard
+            title="this is title"
+            timeToRead="10min"
+            description="this is description hello world!"
+            releaseDate="2023-02-21"
+            hashtags={["hash", "tag"]}
+          />
+          <ContentCard
+            title="this is title"
+            timeToRead="10min"
+            description="this is description hello world!"
+            releaseDate="2023-02-21"
+            hashtags={["hash", "tag"]}
+          />
+          <ContentCard
+            title="this is title"
+            timeToRead="10min"
+            description="this is description hello world!"
+            releaseDate="2023-02-21"
+            hashtags={["hash", "tag"]}
+          />
+          <ContentCard
+            title="this is title"
+            timeToRead="10min"
+            description="this is description hello world!"
+            releaseDate="2023-02-21"
+            hashtags={["hash", "tag"]}
+          />
+          <ContentCard
+            title="this is title"
+            timeToRead="10min"
+            description="this is description hello world!"
+            releaseDate="2023-02-21"
+            hashtags={["hash", "tag"]}
+          />
+          <ContentCard
+            title="this is title"
+            timeToRead="10min"
+            description="this is description hello world!"
+            releaseDate="2023-02-21"
+            hashtags={["hash", "tag"]}
+          />
+        </div>
+
         {paths.map((path) => (
           <Link key={path.id} href={`/posts/${path.id}`}>
             {path.id}
