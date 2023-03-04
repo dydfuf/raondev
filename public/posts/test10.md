b2691f1f3025e07665366abbdcd8eea7ab5853b1
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2023-03-04 23:04:22 +0900

    포스트 날짜 순으로 정렬되도록 수정 (#12)

--- public/posts/test10.md
+++ public/posts/test10.md
@@ -1,6 +1,5 @@
 ---
 title: test10
-date: 2023-02-25
 description: 이 글은 테스트용 입니다.
 category: 테스트, 마크다운, 헬로, world
 ---
--- utils/post.ts
+++ utils/post.ts
@@ -1,10 +1,20 @@
 import fs from "fs";
+import { parseMarkdownMetadata } from "./parseMarkdownMetadata";
+import { isBefore } from "date-fns";
 
 export const getPostNameList = () => {
   try {
     return fs
       .readdirSync(`${process.cwd()}/public/posts`)
-      .map((file) => file.split(".")[0]);
+      .map((file) => file.split(".")[0])
+      .map((postName) => {
+        const { date } = parseMarkdownMetadata(getPostByName(postName));
+        return { postName, date };
+      })
+      .filter((postMetaData) => postMetaData.date !== undefined)
+      .sort((a, b) => +isBefore(new Date(a.date!), new Date(b.date!)))
+      .reverse()
+      .map((postMetaData) => postMetaData.postName);
   } catch {
     return [];
   }
