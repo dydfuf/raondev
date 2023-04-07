d386f77d0467e4c82f35f5685402d32a0a6966f6
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2023-04-07 22:48:15 +0900

    about me 페이지 구현 (#17)

---
+++ components/AboutMe/RoundImageLink.tsx
@@ -0,0 +1,25 @@
+import classNames from "classnames";
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  className?: string;
+  href: string;
+  alt: string;
+  src: string;
+}
+
+export default function RoundImageLink({ className, href, src, alt }: Props) {
+  return (
+    <div
+      className={classNames(
+        "w-full max-w-[160px] rounded-full border-1 aspect-square relative",
+        className
+      )}
+    >
+      <Link href={href}>
+        <Image fill src={src} alt={alt} />
+      </Link>
+    </div>
+  );
+}
