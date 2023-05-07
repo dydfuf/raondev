30fd5e9e0adc3d04b84b806b27189edbc3765671
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2023-05-07 23:13:48 +0900

    home title 추가' (#23)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import MainBanner from "@/components/MainBanner";
 import { parseMarkdownMetadata } from "@/utils/parseMarkdownMetadata";
 import { getPostByName, getPostNameList } from "@/utils/post";
 import { GetStaticProps } from "next";
+import Head from "next/head";
 import readingTime from "reading-time";
 
 interface Props {
@@ -15,32 +16,37 @@ interface Props {
 
 export default function index({ postList }: Props) {
   return (
-    <div>
-      <MainBanner />
-      <DivideLine />
-      <div className="w-full flex justify-center">
-        <div className="w-full gap-20 grid grid-cols-1 p-20 max-w-[768px] sm:grid-cols-2">
-          {postList.map((post) => {
-            const { name, content } = post;
-            const { date, description, category, title } =
-              parseMarkdownMetadata(content);
-            const stats = readingTime(content)
+    <>
+      <Head>
+        <title>{`Raon.dev`}</title>
+      </Head>
+      <div>
+        <MainBanner />
+        <DivideLine />
+        <div className="w-full flex justify-center">
+          <div className="w-full gap-20 grid grid-cols-1 p-20 max-w-[768px] sm:grid-cols-2">
+            {postList.map((post) => {
+              const { name, content } = post;
+              const { date, description, category, title } =
+                parseMarkdownMetadata(content);
+              const stats = readingTime(content)
 
-            return (
-              <ContentCard
-                key={name}
-                title={title ?? ""}
-                timeToRead={stats.text}
-                description={description ?? ""}
-                releaseDate={date ?? ""}
-                categories={category?.split(",") ?? []}
-                to={`/posts/${name}`}
-              />
-            );
-          })}
+              return (
+                <ContentCard
+                  key={name}
+                  title={title ?? ""}
+                  timeToRead={stats.text}
+                  description={description ?? ""}
+                  releaseDate={date ?? ""}
+                  categories={category?.split(",") ?? []}
+                  to={`/posts/${name}`}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
