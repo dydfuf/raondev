d1a4c5c7277c8b97c2aa6a9f09b0d46c62f36986
dydfuf <88dydfuf@naver.com>
2023-05-12 21:39:27 +0900

    open-graph 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,24 @@ import '../styles/globals.css';
 import '../styles/github-markdown.css';
 import { BLOG_TITLE } from '@/constant/common';
 import Analytics from '@/components/Analytics';
+import { Metadata } from 'next';
 
-export const metadata = {
+const description =
+  '안녕하세요. Raon.dev의 개발 블로그 입니다. 주로 Front-end 관련 글을 작성합니다. Youtube 에서 라이브 방송을 합니다.';
+
+export const metadata: Metadata = {
   title: {
     default: BLOG_TITLE,
     template: `${BLOG_TITLE} | %s`,
+  },
+  description,
+  openGraph: {
+    title: BLOG_TITLE,
+    description,
+    url: 'https://raondev.vercel.app',
+    siteName: BLOG_TITLE,
+    locale: 'ko-Kr',
+    type: 'website',
   },
 };
 
