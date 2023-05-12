0776230fefe1edbf96f53a09fd031ad55eca569e
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2023-05-12 20:54:30 +0900

    client component 로 추가 (#31)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import Layout from '@/components/Layout';
 import '../styles/globals.css';
 import '../styles/github-markdown.css';
 import { BLOG_TITLE } from '@/constant/common';
-import { Analytics } from '@vercel/analytics/react';
+import Analytics from '@/components/Analytics';
 
 export const metadata = {
   title: BLOG_TITLE,
