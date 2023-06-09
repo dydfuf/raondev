50374e8b0204b4bd0a71facc77240d1b55c8658d
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2023-06-09 22:33:31 +0900

    todo page 분리 (#45)
    * todo page 분리
    
    * todo md 제거

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,37 @@
-import Image from "next/image";
-import { Anton } from "@next/font/google";
-import classNames from "classnames";
-import Link from "next/link";
+import Image from 'next/image';
+import { Anton } from '@next/font/google';
+import classNames from 'classnames';
+import Link from 'next/link';
 
-const anton = Anton({ subsets: ["latin"], weight: ["400"], preload: false });
+const anton = Anton({ subsets: ['latin'], weight: ['400'], preload: false });
 
 export default function Header() {
   return (
     <header className="h-80 flex border-b-2 items-center px-20 gap-20">
-      <Link href={"/"}>
+      <Link href={'/'}>
         <button className="w-40 h-40 flex items-center justify-center object-contain">
           <Image src="/MainLogo.svg" width={40} height={40} alt="MainLogo" />
         </button>
       </Link>
-      <Link href={"/"}>
+      <Link href={'/'}>
         <span
           className={classNames(
             anton.className,
-            "text-24 tracking-widest hidden mobile:block"
+            'text-24 tracking-widest hidden mobile:block'
           )}
         >
           Raon.dev
         </span>
       </Link>
-      <div className="flex items-center w-full">
+      <div className="flex items-center w-full gap-20">
         {/** TODO: 검색기능 추가 */}
         {/* <div className="flex w-full max-w-[300px] ml-auto mr-20 h-24 border-2 rounded-12 text-12 items-center">
           Search
         </div> */}
-        <Link href="/aboutme" className="ml-auto shrink-0">
+        <Link href="/todo" className="ml-auto shrink-0">
+          <span className="text-16 underline">TODO</span>
+        </Link>
+        <Link href="/aboutme" className="shrink-0">
           <span className="text-16 underline">About me</span>
         </Link>
       </div>
