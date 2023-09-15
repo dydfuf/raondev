1e2405ca115df2a7464fc767b131fde89e2f9e3f
dydfuf <88dydfuf@naver.com>
2023-09-15 21:34:33 +0900

    Header radix-theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,39 +2,47 @@ import Image from 'next/image';
 import { Anton } from '@next/font/google';
 import classNames from 'classnames';
 import Link from 'next/link';
+import { Button, Flex, Text } from '@radix-ui/themes';
 
 const anton = Anton({ subsets: ['latin'], weight: ['400'], preload: false });
 
 export default function Header() {
   return (
-    <header className="h-80 flex border-b-2 items-center px-20 gap-20">
-      <Link href={'/'}>
-        <button className="w-40 h-40 flex items-center justify-center object-contain">
-          <Image src="/MainLogo.svg" width={40} height={40} alt="MainLogo" />
-        </button>
-      </Link>
-      <Link href={'/'}>
-        <span
-          className={classNames(
-            anton.className,
-            'text-24 tracking-widest hidden mobile:block'
-          )}
-        >
-          Raon.dev
-        </span>
-      </Link>
-      <div className="flex items-center w-full gap-20">
-        {/** TODO: 검색기능 추가 */}
-        {/* <div className="flex w-full max-w-[300px] ml-auto mr-20 h-24 border-2 rounded-12 text-12 items-center">
-          Search
-        </div> */}
-        <Link href="/todo" className="ml-auto shrink-0">
-          <span className="text-16 underline">TODO</span>
+    <Flex
+      asChild
+      height={'9'}
+      align={'center'}
+      px={'4'}
+      className="border-1"
+      gap={'3'}
+    >
+      <header>
+        <Link href={'/'}>
+          <button className="w-40 h-40 flex items-center justify-center object-contain">
+            <Image src="/MainLogo.svg" width={40} height={40} alt="MainLogo" />
+          </button>
         </Link>
-        <Link href="/aboutme" className="shrink-0">
-          <span className="text-16 underline">About me</span>
+        <Link href={'/'}>
+          <Text
+            className={classNames(anton.className, 'hidden mobile:block')}
+            size={'7'}
+          >
+            Raon.dev
+          </Text>
         </Link>
-      </div>
-    </header>
+        <Flex align={'center'} width={'100%'} gap={'5'}>
+          <Link href="/todo" className="ml-auto shrink-0">
+            <Button variant="ghost" color="green" size={'3'}>
+              <Text weight={'bold'}>TODO</Text>
+            </Button>
+          </Link>
+          <Link href="/aboutme" className="shrink-0">
+            <Button variant="ghost" color="green" size={'3'}>
+              <Text weight={'bold'}>About Me</Text>
+            </Button>
+          </Link>
+        </Flex>
+      </header>
+    </Flex>
   );
 }
