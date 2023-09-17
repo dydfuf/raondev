28eb86c3512afdd70fdf7ea52c9f20c20e4c7e79
dydfuf <88dydfuf@naver.com>
2023-09-17 17:16:33 +0900

    enable system false

--- app/NextThemeProvider.tsx
+++ app/NextThemeProvider.tsx
@@ -5,7 +5,12 @@ import { PropsWithChildren } from 'react';
 
 export default function NextThemeProvider({ children }: PropsWithChildren) {
   return (
-    <ThemeProvider themes={['dark', 'light']} attribute="class">
+    <ThemeProvider
+      storageKey="themeScheme"
+      enableSystem={false}
+      themes={['dark', 'light']}
+      attribute="class"
+    >
       {children}
     </ThemeProvider>
   );
