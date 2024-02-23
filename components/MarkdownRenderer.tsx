b8dd639e3476cacff3e646229e2aba46160979c0
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2024-02-24 02:35:09 +0900

    post: raon-monorepo 포스트를 작성합니다. (#57)
    * post: 포스트를 작성합니다.
    
    * chore: 링크를 추가합니다.

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -62,7 +62,7 @@ export default function MarkdownRenderer({ markdownStr }: Props) {
         },
         p({ children }: CodeProps) {
           return (
-            <Text as="p" className="!my-16">
+            <Text as="p" className="!my-16 leading-[1.5]" size={'5'}>
               {children}
             </Text>
           );
